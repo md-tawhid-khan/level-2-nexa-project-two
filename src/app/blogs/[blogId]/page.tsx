4c9d2fcbc8b5ee0ad1303375a995169fb220e179
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-19 03:06:19 +0600

    fetch dynamic data using generateStaticParams for ssg

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,17 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+
+export  const generateStaticParams=async()=>{
+    
+    const res=await fetch('http://localhost:5000/blogs')
+    const blogs=await res.json()
+
+    return blogs.slice(0,3).map((blog:Blog)=>({
+        blogId:blog.id
+    }))
+} 
+
 
 const BlogDetailsPage = async({params}:{params:Promise<{blogId:string}>}) => {
     const {blogId}=await params ;
